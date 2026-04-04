89927c8e9b2f77fb75e637447933916fb56822bf
Rakibul-E404 <smrat246@gmail.com>
2026-04-04 12:41:38 +0600

    user image and name functionality fetched__working with profile edit

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:get_storage/get_storage.dart';
 
 
 void main() async {
-  // await GetStorage.init();
+  await GetStorage.init();
   runApp(const TaskManagement());
 
   await SystemChrome.setPreferredOrientations([
